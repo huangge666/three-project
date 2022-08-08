db7339b3b02abb300a7dcb19acb4430fb03fd8c7
Pan.Huang <huang_ge740890944@163.com>
2022-08-08 10:02:51 +0800

    1

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 
 import './assets/scss/base/main.scss';
+import './assets/scss/base/variable.scss';
 
 Vue.config.productionTip = false;
 
